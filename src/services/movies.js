46271b0119c416f48488c15f3163ac72c57491e8
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-04-19 16:19:44 -0300

    fix: change language of banner text

--- src/services/movies.js
+++ src/services/movies.js
@@ -29,7 +29,7 @@ function renderBanner(allMovies) {
     banner.innerHTML += `
     <img src=${backdropUrl} class='banner-image' alt='Banner image' />
 
-    <p class='banner-text'>Find the best movies for you watch!</p>
+    <p class='banner-text'>Encontre os melhores filmes para você assistir!</p>
   `;
   }
 }
